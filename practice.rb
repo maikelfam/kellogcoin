9ae70333b6a11da07fc5695b199c891470bda12e
maikelfam <maikel.fam@kellogg.northwestern.edu>
2022-01-31 05:13:52 +0000

    finished practice

--- practice.rb
+++ practice.rb
@@ -27,3 +27,35 @@ blockchain = [
 # Anthony's KelloggCoin balance is 2650
 
 # 👇👇👇 Your code HERE 👇👇👇
+
+ben_balance = 0
+brian_balance = 0
+evan_balance = 0
+anthony_balance = 0
+
+for transaction in blockchain
+  if transaction [:from_user] == "ben"
+    ben_balance = ben_balance - transaction [:amount]
+  elsif transaction [:from_user] == "brian"
+    brian_balance = brian_balance - transaction [:amount]
+  elsif transaction [:from_user] == "evan"
+    evan_balance = evan_balance - transaction [:amount]
+  elsif transaction [:from_user] == "anthony"
+    anthony_balance = anthony_balance - transaction [:amount]
+  end 
+
+  if transaction [:to_user] == "ben"
+    ben_balance = ben_balance + transaction [:amount]
+  elsif transaction [:to_user] == "brian"
+    brian_balance = brian_balance + transaction [:amount]
+  elsif transaction [:to_user] == "evan"
+    evan_balance = evan_balance + transaction [:amount]
+  elsif transaction [:to_user] == "anthony"
+    anthony_balance = anthony_balance + transaction [:amount]
+  end 
+end 
+
+puts "Ben's KelloggCoin Balance is #{ben_balance}"
+puts "Brian's KelloggCoin Balance is #{brian_balance}"
+puts "Evan's KelloggCoin Balance is #{evan_balance}"
+puts "Anthony's KelloggCoin Balance is #{anthony_balance}"
